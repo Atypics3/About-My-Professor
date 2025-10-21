ea23bcaf2e395d81fda175ae0c92012238a7ee8f
B1ggestB <soccerb743@gmail.com>
2025-10-21 13:48:23 -0700

    got names from panels in the DOM
    can get names per panel or get an array of all names but need to pass the name (which can be converted into a uID) to the component

--- src/content/content.js
+++ src/content/content.js
@@ -3,11 +3,42 @@ import { createRoot } from "react-dom/client";
 import ProfInfoButton from "../react/components/ProfInfoButton.jsx";
 
 function renderIntoPanels() {
+  /*
+  // get the prof names from the DOM
+  let names = [];
+  for (let i = 0; i<25; i++){
+    let profNameElement = document.getElementById("rowpanel_" + i);
+    //id: "rowpanel_0-24" 
+    const re = /Instructor:\n (.*)\n/;
+    if (profNameElement) {
+        //console.log(profNameElement.outerText)
+        //send to background.js to do regex? seems like we don't need to
+        let text = profNameElement.outerText
+        let res = (text).match(re);
+        let name = res[1];
+        names.push(name);
+        //console.log(name);
+        //chrome.runtime.sendMessage({ greeting : text });
+    }  
+  }
+  //console.log(names);
+  */
+
   const panels = document.querySelectorAll(".panel.panel-default.row");
   if (!panels || panels.length === 0) return;
 
   panels.forEach((panel) => {
     if (panel.querySelector(".about-my-professor-root")) return; // avoid duplicate mounts(will come in handy when we cache the results)
+
+    //mabye get name from panel
+    // console.log(panel.innerText) - use this instead
+    const re = /Instructor:\n (.*)\n/;
+    //send to background.js to do regex? seems like we don't need to
+    let text = panel.innerText;
+    let res = text.match(re);
+    let name = res[1];
+    console.log(name);
+    //how to send this to the component
 
     const mount = document.createElement("div");
     mount.className = "about-my-professor-root";
