64d68873b23fbf1ad9ad167a61638e15511c0129
B1ggestB <soccerb743@gmail.com>
2025-10-22 14:33:59 -0700

    moved api fetching to background scprit
    still needs to be cleaned up

--- src/react/components/ProfInfoButton.jsx
+++ src/react/components/ProfInfoButton.jsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "../styles/index.css";
-import Axios from "axios"; //for fetching from the API do npm install axios if this errors locally
-import data from "../../data/prof_uids.json";
 
 export default function ProfInfoButton(props) {
   // Track whether popup is open or closed
@@ -11,31 +9,14 @@ export default function ProfInfoButton(props) {
 
   //useEffect so this isn't run every time page reloads
   useEffect(() => {
-    //get abreviated name from content.js
-    let name = props.profName;
-    //console.log(name);
-    let uID = "jdoe";
-    //console.log(data);
-    //get uID by indexing the json data as a dictionary
-    if (data[name]) {
-      uID = data[name];
+    //get fullname from content.js
+    //console.log(props.apiData);
+    if (props.apiData != null) {
+      let name = props.apiData.cn;
+      //console.log(name);
+      getFullName(name);
     } else {
-      console.log(
-        "couldn't match name to uID in the json, gave output",
-        data[name],
-      );
-    }
-
-    if (uID == "jdoe") {
-      //if couldn't get uID set text to John Doe
       getFullName("John Doe");
-    } else {
-      //axios instead of fetch to get the full name from the api
-      Axios.get("https://campusdirectory.ucsc.edu/api/uid/" + uID).then(
-        (res) => {
-          getFullName(res.data.cn[0]);
-        },
-      );
     }
   }, []);
 
